3ce713e31cb13df253f49eb7219f81aaa1f5a5fe
mrveldkamp <37160046+mrveldkamp@users.noreply.github.com>
2023-09-04 09:51:56 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,51 +1,58 @@
 // BRIGHTNESS IPO CALC BY MR. V
 
+// HTML Variables
+let rInEl = document.getElementById("r-in");
+let gInEl = document.getElementById("g-in");
+let bInEl = document.getElementById("b-in");
+let brightEl = document.getElementById("bright");
+let previewEl = document.getElementById("preview");
+
 // Button Click Event
 document.getElementById("btn").addEventListener("click", btnClicked);
 
 function btnClicked() {
   // Get Input Values
-  let r = +document.getElementById("r-in").value;
-  let g = +document.getElementById("g-in").value;
-  let b = +document.getElementById("b-in").value;
+  let r = +rInEl.value;
+  let g = +gInEl.value;
+  let b = +bInEl.value;
 
   // Validate Input Values - rgb values must be between 0 and 255
   if (r < 0) {
     r = 0;
-    document.getElementById("r-in").value = 0;
+    rInEl.value = 0;
   } else if (r > 255) {
     r = 255;
-    document.getElementById("r-in").value = 255;
+    rInEl.value = 255;
   }
 
   if (g < 0) {
     g = 0;
-    document.getElementById("g-in").value = 0;
+    gInEl.value = 0;
   } else if (g > 255) {
     g = 255;
-    document.getElementById("g-in").value = 255;
+    gInEl.value = 255;
   }
 
   if (b < 0) {
     b = 0;
-    document.getElementById("b-in").value = 0;
+    bInEl.value = 0;
   } else if (b > 255) {
     b = 255;
-    document.getElementById("b-in").value = 255;
+    bInEl.value = 255;
   }
 
   // Calculate & Output Brightness
   let brightness = Math.sqrt(0.299 * r ** 2 + 0.587 * g ** 2 + 0.114 * b ** 2);
   brightness = Math.round(brightness);
-  document.getElementById("bright").innerHTML = brightness;
+  brightEl.innerHTML = brightness;
 
   // Update rgb preview: brightness > 160 -> black text, else white text
   let rgbString = `rgb(${r}, ${g}, ${b})`;
-  document.getElementById("rgb-preview").innerHTML = rgbString;
-  document.getElementById("rgb-preview").style.backgroundColor = rgbString;
+  previewEl.innerHTML = rgbString;
+  previewEl.style.backgroundColor = rgbString;
   if (brightness > 160) {
-    document.getElementById("rgb-preview").style.color = "black";
+    previewEl.style.color = "black";
   } else {
-    document.getElementById("rgb-preview").style.color = "white";
+    previewEl.style.color = "white";
   }
 }
